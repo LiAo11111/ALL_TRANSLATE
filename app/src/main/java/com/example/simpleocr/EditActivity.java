eb29d0bf497345c1a10fc72152ad187ebcd4481a
Water29 <598751657@qq.com>
2024-06-20 16:18:50 +0800

    edit 修改

--- app/src/main/java/com/example/simpleocr/EditActivity.java
+++ app/src/main/java/com/example/simpleocr/EditActivity.java
@@ -16,30 +16,44 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.util.Objects;
 
 /**
- * @author 30415
+ * 编辑文本的活动。
+ * 提供一个文本输入框并处理返回按钮和菜单操作。
+ * 使用 Material Design 组件进行动态颜色主题设置。
  */
 public class EditActivity extends AppCompatActivity {
 
+    // 文本输入框，用于编辑文本
     TextInputEditText textInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 如果可用，应用动态颜色
         DynamicColors.applyToActivityIfAvailable(this);
+
+        // 设置状态栏颜色
         getWindow().setStatusBarColor(SurfaceColors.SURFACE_2.getColor(this));
+
+        // 禁用返回按钮的默认行为
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
+
+        // 设置活动的布局
         setContentView(R.layout.activity_edit);
 
+        // 创建并添加返回按钮回调
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-
+                // 此处可以添加返回按钮被按下时的处理逻辑
             }
         };
         getOnBackPressedDispatcher().addCallback(this, callback);
 
+        // 获取布局中的文本输入框
         textInputEditText = findViewById(R.id.editText);
 
+        // 如果Intent中包含"text"数据，则设置文本输入框的内容
         if (null != getIntent().getStringExtra("text")) {
             textInputEditText.setText(getIntent().getStringExtra("text"));
         }
@@ -47,16 +61,23 @@ public class EditActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // 加载菜单布局
         getMenuInflater().inflate(R.menu.done, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // 处理菜单项被选中的情况
         if (item.getItemId() == R.id.done) {
+            // 获取文本输入框的内容
             CharSequence text = textInputEditText.getText();
+
+            // 创建Intent并将文本内容放入其中
             Intent intent = new Intent();
             intent.putExtra("text", text);
+
+            // 设置结果并结束活动
             setResult(Activity.RESULT_OK, intent);
             finish();
             return true;
